4bf13e4319ce10f4e035054b9536f28a6322fa98
AlvaroSegovi <137243412+AlvaroSegovi@users.noreply.github.com>
2023-08-01 13:52:06 -0600

    Agregué el Script del Firewall

---
+++ ScriptFirewall.ps1
@@ -0,0 +1,40 @@
+
+# Crear un grupo de recursos
+az group create --name FWRG --location eastus
+
+#Crear la VNet FWVN y la subnet AzureFirewallSubnet
+az network vnet create --name FWVN --resource-group FWRG --location eastus --address-prefixes 10.0.0.0/16 --subnet-name AzureFirewallSubnet --subnet-prefixes 10.0.1.0/24
+
+#Crear la subred FWSN1
+az network vnet subnet create --name FWSN1 --vnet-name FWVN --resource-group FWRG --address-prefixes "10.0.2.0/24"
+
+#Crear la subred FWSN2
+az network vnet subnet create --name FWSN2 --vnet-name FWVN --resource-group FWRG --address-prefixes "10.0.3.0/24"
+
+#Crear maquina virtual VMpublica en  la subred FWSN2
+az vm create --name VMpublica --resource-group FWRG --location eastus  --image Win2019Datacenter --admin-username alvaro  --size Standard_B1s --os-disk-size-gb 130 --os-disk-name DiscoVMpublica --vnet-name FWVN --subnet FWSN2 
+
+#Abrir el puerto RDP 3389
+az vm open-port --name VMpublica --resource-group FWRG --port 3389  
+
+#Crear la NIC para VMprivada sin ip pública.
+ az network nic create --name NicVMprivada --resource-group FWRG --location eastus --vnet-name FWVN --subnet FWSN1
+
+#Crear maquina virtual VMprivada en la subred FWSN2
+az vm create --name VMprivada --resource-group FWRG --nics NicVMprivada --location eastus  --image Win2019Datacenter --admin-username alvaro --size Standard_B1s --os-disk-size-gb 130 --os-disk-name DiscoVMprivada
+ # When specifying an existing NIC, do not specify NSG, public IP, ASGs, VNet or subnet.
+ 
+#Crear la ip pública (Fuera del Firewall)
+az network public-ip create --name ippublic1  --resource-group FWRG --sku Standard --allocation-method Static
+
+# Configurar la ip publica dentro del firewall, si no se hace esto no se puede ver la configuracion ip del firewall (AzureFirewall/Public IP configuration) y no se puede conectar con escritorio remoto. 
+az network firewall ip-config create --firewall-name AzureFirewall --name publicipconf --public-ip-address ippublic1 --resource-group FWRG --vnet-name FWVN
+
+#Implementar el Firewall
+az network firewall create --name AzureFirewall --resource-group FWRG --location eastus --sku AZFW_VNet --vnet-name AzureFirewallSubnet --public-ip ippublic1
+
+#Crear la regla NAT
+az network firewall nat-rule create --firewall-name AzureFirewall --resource-group FWRG --collection-name Regla1RDP --priority 101 --action DNAT --name vmaccess --source-addresses 170.84.135.64 --protocols TCP --destination-ports 3500 --destination-addresses 20.232.174.4 --translated-address 10.0.2.4 --translated-port 3389
+
+#Actualizar el firewall
+az network firewall update --name AzureFirewall --resource-group FWRG
